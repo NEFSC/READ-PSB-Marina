45a82b1dce9ecae5cf5724d0b2316c9f66eb835e
namariD <50114391+namariD@users.noreply.github.com>
2020-10-26 11:53:07 -0400

    Add files via upload
    Just changed the ordering of the lines to be more efficient

--- Files to aggregate daily hourly data/readWMD_SQLiteTable_2inputs.m
+++ Files to aggregate daily hourly data/readWMD_SQLiteTable_2inputs.m
@@ -1,6 +1,6 @@
 function wmd= readWMD_SQLiteTable_2inputs(pathname,filename,species)
 %User provides path and filename of a Pamguard database from which the
-%humpback and dolphin Whistle and Moan detector table can be extracted
+%humpback or dolphin Whistle and Moan detector table can be extracted
 
 %table names in the SQL database have to be named Humpback_WMD and
 %Dolphin_WMD
@@ -10,6 +10,7 @@ function wmd= readWMD_SQLiteTable_2inputs(pathname,filename,species)
 
 %Annamaria DeAngelis
 %10/23/20
+%modified 10/26/2020 to reflect renaming SQL database table
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %'C:\Users\annamaria.deangelis.NMFS\Documents\SanctSound\Dolphin_PG_Detector\ToBeCopied\SanctSound_FK01_01_dolphins - Copy.sqlite3
@@ -17,13 +18,13 @@ function wmd= readWMD_SQLiteTable_2inputs(pathname,filename,species)
 %read in SQLite table
 dbid = mksqlite(0, 'open', [pathname,filename], 'ro');
 
-humpback= mksqlite(dbid, 'SELECT * FROM Humpback_WMD ORDER BY UTC'); 
-dolphin= mksqlite(dbid, 'SELECT * FROM Dolphin_WMD ORDER BY UTC');
-humpback= struct2table(humpback);
-dolphin= struct2table(dolphin);
 if strcmp(species,'humpback')
+    humpback= mksqlite(dbid, 'SELECT * FROM Humpback_WMD ORDER BY UTC');
+    humpback= struct2table(humpback);
     wmd= humpback;
 else
+    dolphin= mksqlite(dbid, 'SELECT * FROM Dolphin_WMD ORDER BY UTC');
+    dolphin= struct2table(dolphin);
     wmd= dolphin;
 end
 
