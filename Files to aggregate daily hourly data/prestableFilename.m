80c6f5a85472da2954e7472f3f6b0655ed4d3ace
Allison S <71660890+allison-stokoe@users.noreply.github.com>
2021-04-16 03:38:52 -0400

    a short function that creates filenames for prestables

---
+++ Files to aggregate daily hourly data/prestableFilename.m
@@ -0,0 +1,12 @@
+function newfilename = prestableFilename(pathname,projectname,species)
+%For aggregate data GUI. Works with SanctSound file formats to creat new
+%name for exported presence tables. 
+% Allison Stokoe 4/16/2021
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+ newfilename = strsplit(projectname,'_');
+ newfilename = {char(newfilename{1}), char(newfilename{2}), char(newfilename{3})};
+ newfilename = strjoin(newfilename, '_');
+ newfilename = newfilename(~isspace(newfilename));
+ newfilename = [char(pathname),char(newfilename),'_', char(species),'.xlsx'];
+end
+
